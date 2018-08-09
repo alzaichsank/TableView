21c2c094eb6f96ede835d2c77c4fbd91902af364
Alza Devloper <alzaichsank@gmail.com>
2018-08-10 00:07:37 +0700

    AK: add dummy list

---
+++ app/src/main/java/e/alzaichsank/view_table/ListTableActivity.kt
@@ -0,0 +1,12 @@
+package e.alzaichsank.view_table
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListTableActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_table)
+    }
+}
